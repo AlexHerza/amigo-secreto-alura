e7b51572b8d30c957397651f00b2349683145b63
Alexandra Hernández <alexherzarate@gmail.com>
2025-02-16 00:04:48 -0500

    modificando el app.js

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 // Array para almacenar los nombres de los amigos
 
 let amigos = [];
+
+// Función para agregar amigo
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value.trim();
+
+    
+    // Validaciones
+    if (!nombreAmigo) return alert("Por favor, ingresa un nombre.");
+    if (!/^[a-zA-Z]+$/.test(nombreAmigo)) return alert("Estas seguro que es un nombre?");
+    if (amigos.includes(nombreAmigo)) return alert("Ese amigo ya está en la lista.");
+
+    amigos.push(nombreAmigo);
+    inputAmigo.value = "";
+    actualizarLista();
+}
+
+// Función para actualizar la lista en la interfaz
+function actualizarLista() {
+let listaAmigos = document.getElementById("listaAmigos");
+listaAmigos.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
+}
+
+// Función para sortear un amigo aleatorio
+function sortearAmigo() {
+if (!amigos.length) return alert("No hay amigos en la lista para jugar.");
+let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+document.getElementById("resultado").innerHTML = `<strong>🎉 El amigo secreto es: ${amigoSorteado} 🎉</strong>`;
+
+
+}
